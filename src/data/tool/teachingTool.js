2a097b98944b28a481c2193e9bc94ef3d58896d6
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:37 +0800

    Create teachingTool.js

---
+++ src/data/tool/teachingTool.js
@@ -0,0 +1,24 @@
+// 教学模式
+const menu = [
+  {
+    text: '',
+    childList: [
+      {
+        childText: '资源浏览',
+        childIcon: 'el-icon-s-platform',
+        iconColor: '#409EFF',
+        target: 'iframe',
+        url: 'http://pdf.car.com/#/'
+      },
+      {
+        childText: '监测系统',
+        childIcon: 'el-icon-s-marketing',
+        iconColor: '#409EFF',
+        target: 'iframe',
+        url: '/admin/curriculum/resources_views/'
+      }
+    ]
+  }
+]
+
+export { menu }
