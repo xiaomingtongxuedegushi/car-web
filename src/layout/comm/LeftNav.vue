4ff8a575be52a825106b591ea3dd2b038664f7a6
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:47 +0800

    Create LeftNav.vue

---
+++ src/layout/comm/LeftNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+  name: 'LeftNav',
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
