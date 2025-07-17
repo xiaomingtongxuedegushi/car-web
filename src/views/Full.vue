06015060e0ef971dc8a282e527c370aed8d0322d
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:53:16 +0800

    Create Full.vue

---
+++ src/views/Full.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="mc-page-contianer">
+      <div class="mc-page-left">
+        侧边栏
+      </div>
+      <div class="mc-page-right">
+        右侧区域
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Full'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
