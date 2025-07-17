a3f76a4c0263b0bf6a570a58bb7854164dbb6873
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:59 +0800

    Create LeftHeader.vue

---
+++ src/layout/LeftHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LeftNav />
+  </div>
+</template>
+
+<script>
+import LeftNav from '@/layout/comm/LeftNav'
+export default {
+  name: 'Header',
+  components: { LeftNav }
+}
+</script>
+
+<style scoped>
+
+</style>
