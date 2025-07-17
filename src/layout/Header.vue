e45cb275b723c24a35bc21231bc0e370069ea10f
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:55 +0800

    Create Header.vue

---
+++ src/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nav />
+  </div>
+</template>
+
+<script>
+import Nav from '@/layout/comm/Nav'
+export default {
+  name: 'Header',
+  components: { Nav }
+}
+</script>
+
+<style scoped>
+
+</style>
