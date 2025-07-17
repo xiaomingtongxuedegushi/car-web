9ee80cede8d5f4856eac43fd744d5c256787a289
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:27 +0800

    Create theme.js

---
+++ src/data/theme/utils/theme.js
@@ -0,0 +1,51 @@
+const template = {
+  // 侧边栏
+  leftBar: {
+    // 侧边栏标题
+    title: {
+      color: null
+    },
+    // 侧边栏背景
+    background: {
+      color: null
+    },
+    // 侧边栏一级菜单配置
+    menu1: {
+      text: {
+        color: null
+      }
+    },
+    // 侧边栏二级菜单配置
+    menu2: {
+      // 二级菜单项颜色配置
+      item: {
+        text: {
+          color: null
+        },
+        background: {
+          color: null
+        }
+      },
+      // 鼠标悬浮后变化
+      hover: {
+        text: {
+          color: null
+        },
+        background: {
+          color: null
+        }
+      },
+      // 被激活的菜单
+      active: {
+        text: {
+          color: null
+        },
+        background: {
+          color: null
+        }
+      }
+    }
+  }
+}
+
+export { template }
