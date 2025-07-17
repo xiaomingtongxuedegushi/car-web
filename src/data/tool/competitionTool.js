da0026ced65579f2ebcf2fa434245e87b74be0b6
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:32 +0800

    Create competitionTool.js

---
+++ src/data/tool/competitionTool.js
@@ -0,0 +1,17 @@
+// 竞赛模式
+const menu = [
+  {
+    text: '',
+    childList: [
+      {
+        childText: '竞赛系统',
+        childIcon: 'el-icon-trophy',
+        iconColor: '#409EFF',
+        target: 'iframe',
+        url: 'http://ele.car.com/#/'
+      }
+    ]
+  }
+]
+
+export { menu }
