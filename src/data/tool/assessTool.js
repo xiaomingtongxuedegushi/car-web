a8de9f6dcb7d91bbdf69023a893d31abe152a02f
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:29 +0800

    Create assessTool.js

---
+++ src/data/tool/assessTool.js
@@ -0,0 +1,17 @@
+// 考核模式
+const menu = [
+  {
+    text: '',
+    childList: [
+      {
+        childText: '参考资料',
+        childIcon: 'el-icon-reading',
+        iconColor: '#409EFF',
+        target: 'iframe',
+        url: 'http://pdf.car.com/#/'
+      }
+    ]
+  }
+]
+
+export { menu }
