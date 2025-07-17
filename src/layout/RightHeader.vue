5f3fa95bcc35ef1e6ee51c1b3970d7f38d2457f1
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:53:03 +0800

    Create RightHeader.vue

---
+++ src/layout/RightHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <RightNav />
+  </div>
+</template>
+
+<script>
+import RightNav from '@/layout/comm/RightNav'
+export default {
+  name: 'Header',
+  components: { RightNav }
+}
+</script>
+
+<style scoped>
+
+</style>
