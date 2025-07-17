0def9116eebd030ab11800d0b50d72826cac0d73
xiaomingtongxuedegushi <1299068558@qq.com>
2025-07-17 11:52:40 +0800

    Create trainingTool.js

---
+++ src/data/tool/trainingTool.js
@@ -0,0 +1,17 @@
+// 训练模式
+const menu = [
+  {
+    text: '训练系统',
+    childList: [
+      {
+        childText: '训练工单',
+        childIcon: 'el-icon-s-order',
+        iconColor: '#409EFF',
+        target: 'iframe',
+        url: 'http://ele.car.com'
+      }
+    ]
+  }
+]
+
+export { menu }
